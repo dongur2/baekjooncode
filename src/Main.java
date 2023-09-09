8fb3a756d82b395bde75e3536b2257bca9079098
dongur2 <holymolyguri@gmail.com>
2023-09-09 23:30:21 +0900

    step10 1~4

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,15 @@ import seven.Coloredpaper;
 import seven.MatrixAddition;
 import seven.Max;
 import seven.VerticalReading;
+import ten.EscapeFromRectangle;
+import ten.FourthSpot;
+import ten.MathIsPE;
+import ten.Rectangle;
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        PrimeFactorization.getResult();
+        MathIsPE.getResult();
     }
 }
