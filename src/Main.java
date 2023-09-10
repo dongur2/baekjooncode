6d60b486817ee40935265f32a0521f976e10b47c
dongur2 <holymolyguri@gmail.com>
2023-09-10 15:49:12 +0900

    step10 finished

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,9 @@
-import eight.*;
-import nine.*;
-import seven.Coloredpaper;
-import seven.MatrixAddition;
-import seven.Max;
-import seven.VerticalReading;
-import ten.EscapeFromRectangle;
-import ten.FourthSpot;
-import ten.MathIsPE;
-import ten.Rectangle;
+import ten.*;
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        MathIsPE.getResult();
+        ThreeSticks.getResult();
     }
 }
