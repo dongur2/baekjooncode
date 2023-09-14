3295c16ffbd3bde360d8f0391f67d1ab9c3d7431
dongur2 <holymolyguri@gmail.com>
2023-09-15 01:06:03 +0900

    step12-5,6 finished

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,9 @@
-import twelve.Blackjack;
-import twelve.MathIsOnlineClass;
-import twelve.PaintChessBoard;
-import twelve.SumOfNums;
+import twelve.*;
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException{
-        PaintChessBoard.getResult();
+        SugarDelivery.getResult();
     }
 }
