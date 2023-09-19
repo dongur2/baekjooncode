60ac076f25efe312f863e265cdcdda68b210bcbd
dongur2 <holymolyguri@gmail.com>
2023-09-20 05:37:19 +0900

    class1++ 1Q solved

--- src/classes/class1/class1pp/Hotel.java
+++ src/classes/class1/class1pp/Hotel.java
@@ -16,15 +16,18 @@ public class Hotel {
             int W = Integer.parseInt(input.nextToken());
             int N = Integer.parseInt(input.nextToken());
 
-            int floor = N % H;
-            if (floor == 0) {
-                floor = H;
+            int floor;
+            if (N % H == 0) { // N이 H의 배수일 경우 나머지는 0이 되지만, 층수는 H층이어야 하므로 따로 H로 지정
+                floor = H * 100;
+            } else {
+                floor = (N % H) * 100;
             }
-            floor = floor * 100;
 
-            int number = (N / H);
-            if (number == 0) {
-                number = N / H + 1;
+            int number;
+            if (N % H == 0) { // N이 H의 배수일 경우 N/H==1이 되므로, 1+1이 되면 식이 틀리게 된다. 그러므로 따로 지정
+                number = N / H;
+            } else {
+                number = (N / H) + 1;
             }
 
             System.out.println(floor+number);
